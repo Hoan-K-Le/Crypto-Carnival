ced2e2dd0f726dbf0ced6fc3746dad4bff31cc41
hoan <103342095+Hoan-K-Le@users.noreply.github.com>
2023-09-19 16:01:15 -0700

    initializing files again

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: ["class"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        white100: "#ffffff",
+        grey100: "#FCFCFC",
+        green200: "#00FC2A",
+        slate700: "#2C2F36",
+        slate800: "#1F2128",
+        slate900: "#191b1f",
+        'lightning': "#1e1932"
+      },
+      maxWidth: {
+        "3.5xl": "52.1875rem",
+        "8xl": "118.75rem",
+      },
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px',
+        '3': '3px',
+        '4': '4px'
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
+};
+export default config;
