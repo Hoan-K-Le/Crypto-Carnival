30b09b49d7099d6ac7775ed8b51760334b1f9274
hoan <103342095+Hoan-K-Le@users.noreply.github.com>
2023-09-19 16:02:11 -0700

    added store file to implement the reducers

---
+++ app/store/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { useSelector, TypedUseSelectorHook } from "react-redux";
+import currencyReducer from "./CurrencyReducer";
+import coinsReducer from "./CoinsData";
+import globalCoinReducer from "./CoinsBarData";
+
+export const store = configureStore({
+  reducer: {
+    currency: currencyReducer,
+    coins: coinsReducer,
+    generalCoins: globalCoinReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
