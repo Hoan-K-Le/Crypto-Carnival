5a179f6fb77ad748768d3670ab352399e432bb21
hoan <103342095+Hoan-K-Le@users.noreply.github.com>
2023-09-21 11:48:30 -0700

    removed commented out code

--- app/components/TableOverview/VolumeMarket/index.tsx
+++ app/components/TableOverview/VolumeMarket/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { TableDataProps } from '../TableDataProps'
-import { useAppSelector } from '@/app/store/store'
+import React from "react";
+import { TableDataProps } from "../TableDataProps";
+import { useAppSelector } from "@/app/store/store";
 
 interface VolumeMarketProps {
-  coin: TableDataProps
-  formatNumber: (number: number) => string
-  symbol: string
+  coin: TableDataProps;
+  formatNumber: (number: number) => string;
+  symbol: string;
 }
 
 const VolumeMarket: React.FC<VolumeMarketProps> = ({
@@ -19,8 +19,7 @@ const VolumeMarket: React.FC<VolumeMarketProps> = ({
         <div className="bg-green-300 rounded-full h-[10px] w-[10px]"></div>
         <span>
           {symbol}
-          {/* {formatNumber(coin.total_volume)} */}
-          {coin.total_volume}
+          {formatNumber(coin.total_volume)}
         </span>
       </div>
       <div>
@@ -28,13 +27,12 @@ const VolumeMarket: React.FC<VolumeMarketProps> = ({
           <div className="bg-yellow-300 rounded-full h-[10px] w-[10px]"></div>
           <span>
             {symbol}
-            {/* {formatNumber(coin.market_cap_change_24h)} */}
-            {coin.market_cap_change_24h}
+            {formatNumber(coin.market_cap_change_24h)}
           </span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default VolumeMarket
+export default VolumeMarket;
