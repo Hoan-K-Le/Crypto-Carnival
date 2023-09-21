d1b0ac69a879d409ff0a4dcad02766f6ddc189eb
hoan <103342095+Hoan-K-Le@users.noreply.github.com>
2023-09-21 11:55:09 -0700

    added the side components that is needed to refactor the code

---
+++ app/components/Buttons/Theme/Theme.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import Icon from "../../Icon/Icon";
+
+export default function ThemeButton() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <button
+      className="bg-[#CCCCFA] bg-opacity-40 text-[#424286] dark:bg-[#191925] dark:text-white da p-3.5 rounded-lg"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {theme === "dark" ? (
+        <Icon iconVariant="sun" />
+      ) : (
+        <Icon iconVariant="moon" />
+      )}
+    </button>
+  );
+}
---
+++ app/components/Icon/Icon.tsx
@@ -0,0 +1,55 @@
+import { CiSearch } from "react-icons/ci";
+import {
+  PiCurrencyDollarSimpleBold,
+  PiCaretUpBold,
+  PiCaretDownBold,
+  PiCurrencyGbpLight,
+  PiSwapFill,
+} from "react-icons/pi";
+import {
+  BiSolidChevronDown,
+  BiDollar,
+  BiEuro,
+  BiHomeAlt2,
+} from "react-icons/bi";
+
+import { RiHome5Fill } from "react-icons/ri";
+import {
+  BsSunFill,
+  BsLightningChargeFill,
+  BsStack,
+  BsMoon,
+} from "react-icons/bs";
+import { ImStack } from "react-icons/im";
+
+type Icons = {
+  [key: string]: React.ElementType<any>;
+};
+
+const icons: Icons = {
+  search: CiSearch,
+  dollar: PiCurrencyDollarSimpleBold,
+  chevDown: BiSolidChevronDown,
+  sun: BsSunFill,
+  moon: BsMoon,
+  arrowUp: PiCaretUpBold,
+  arrowDown: PiCaretDownBold,
+  euro: BiEuro,
+  gbp: PiCurrencyGbpLight,
+  lightning: BsLightningChargeFill,
+  swap: PiSwapFill,
+  portfolio: ImStack,
+  portfolioSolid: BsStack,
+  home: BiHomeAlt2,
+  homeSolid: RiHome5Fill,
+};
+
+type props = {
+  iconVariant: string;
+  className?: string;
+};
+
+export default function Icon({ iconVariant, className }: props) {
+  const IconVariant = icons[iconVariant];
+  return <IconVariant className={className} type={iconVariant} />;
+}
