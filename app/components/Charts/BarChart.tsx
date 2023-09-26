d63a362d2f784ba04ace3d57876ec5db98978e29
hoan <103342095+Hoan-K-Le@users.noreply.github.com>
2023-09-25 23:20:45 -0700

    refactored the top charts

--- app/components/Charts/BarChart.tsx
+++ app/components/Charts/BarChart.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -7,10 +7,12 @@ import {
   LinearScale,
   BarElement,
 } from "chart.js";
-import { getBitcoinData } from "@/app/contexts/Charts";
-import { useAppSelector } from "@/app/store/store";
-ChartJS.register(CategoryScale, LinearScale, BarElement);
 
+import { useAppSelector, AppDispatch } from "@/app/store/store";
+import { useDispatch } from "react-redux";
+import { fetchGraphData } from "@/app/store/ChartSelectorData";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement);
 const options = {
   responsive: true,
   plugins: {
@@ -21,11 +23,11 @@ const options = {
   scales: {
     x: {
       grid: {
-        display: false, // Removes the background grid lines for X-axis
+        display: false,
       },
     },
     y: {
-      display: false, // Removes the numbers for the y-axis
+      display: false,
     },
   },
 };
@@ -33,19 +35,43 @@ const options = {
 export default function BarChart() {
   const [bitcoinPriceVolumes, setBitcoinPriceVolumes] = useState<number[]>([]);
   const [bitcoinVolumeDates, setBitcoinVolumeDates] = useState<number[]>([]);
-  const [currentDate, setCurrentDate] = useState<string>("");
+  const [currentData, setCurrentData] = useState<string>([]);
+  const [
+    gradientBackground,
+    setGradientBackground,
+  ] = useState<CanvasGradient | null>(null);
   const currentCurrency = useAppSelector(state => state.currency.currencies);
+  const isLoading = useAppSelector(state => state.coinGraph.isLoading);
+  const dispatch = useDispatch<AppDispatch>();
+  const chartRef = useRef();
   const fetchChartData = async () => {
     try {
-      const chartData = await getBitcoinData(currentCurrency);
-      const Volumes = chartData.total_volumes.map(
-        (volumes: [number, number]) => volumes[1]
+      const btc = "bitcoin";
+      const chartData = await dispatch(
+        fetchGraphData({
+          currency: currentCurrency,
+          name: btc,
+        })
       );
-      const dates = chartData.total_volumes.map(
-        (volumes: [number, number]) => volumes[0]
-      );
-      setBitcoinPriceVolumes(Volumes);
-      setBitcoinVolumeDates(dates);
+
+      if (fetchGraphData.fulfilled.match(chartData)) {
+        const currentDate = new Date();
+        const getCurrentData = chartData.payload.total_volumes.filter(
+          (volumeData: [number, number]) => {
+            const volumeDate = new Date(volumeData[0]);
+            return currentDate.toDateString() === volumeDate.toDateString();
+          }
+        );
+        setCurrentData(getCurrentData);
+        const volume = chartData.payload.total_volumes.map(
+          (volume: [number, number]) => volume[1]
+        );
+        const date = chartData.payload.total_volumes.map(
+          (volumeDate: [number, number]) => volumeDate[0]
+        );
+        setBitcoinPriceVolumes(volume);
+        setBitcoinVolumeDates(date);
+      }
     } catch (err) {
       console.log(err);
     }
@@ -53,10 +79,15 @@ export default function BarChart() {
 
   useEffect(() => {
     fetchChartData();
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    if (ctx) {
+      const gradient = ctx.createLinearGradient(0, 0, 0, 400);
+      gradient.addColorStop(0, "rgba(157, 98, 217, 1)");
+      gradient.addColorStop(1, "rgba(179, 116, 242, 0.01)");
+      setGradientBackground(gradient);
+    }
   }, []);
-
-  // const currentTheme = localStorage.getItem("theme");
-  // const backgroundColor = currentTheme === "dark" ? "#3D63EC" : "#00FC2A";
 
   const data = {
     labels: bitcoinVolumeDates.map(date => new Date(date).getDate()),
@@ -65,14 +96,31 @@ export default function BarChart() {
         fill: true,
         label: "Volumes",
         data: bitcoinPriceVolumes,
-        // backgroundColor: backgroundColor,
+        backgroundColor: gradientBackground as any,
       },
     ],
   };
 
   return (
-    <div>
-      <Bar data={data} options={options} />
+    <div className="h-[400px] w-full">
+      <div className="flex flex-col gap-2">
+        <p className="text-xl text-[#191932]">Volume 24h</p>
+        <p className="text-3xl text-[#181825]">
+          {currentData.length > 0
+            ? Number(currentData[0][1]).toLocaleString("en-US")
+            : "Loading..."}
+        </p>
+        <p className="text-[#424286]">
+          {currentData.length > 0
+            ? new Date(currentData[0][1]).toDateString()
+            : "Loading..."}
+        </p>
+      </div>
+      {isLoading ? (
+        <div>Loading Graphs...</div>
+      ) : (
+        <Bar ref={chartRef} data={data} options={options} />
+      )}
     </div>
   );
 }
