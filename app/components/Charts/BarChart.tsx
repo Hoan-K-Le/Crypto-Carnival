5a0897d258206330c6b3882507723bdf003064d4
hoan <103342095+Hoan-K-Le@users.noreply.github.com>
2023-11-01 09:17:40 -0700

    test

--- app/components/Charts/BarChart.tsx
+++ app/components/Charts/BarChart.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS, CategoryScale, BarElement } from "chart.js";
 import { useDispatch } from "react-redux";
